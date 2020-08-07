e5708b0f8057c668c6d7ace31f78faa02ff8ba1c
Eric Kupfer <erkupfer@microsoft.com>
2020-08-07 12:25:30 -0500

    update deployment prompts in deploy script

--- createNewVmFromImage/deploy.ps1
+++ createNewVmFromImage/deploy.ps1
@@ -1,11 +1,11 @@
-$deploymentRG = Write-Host "Enter Resource Group new VM's will be deployed to:"
-$VMNamePrefix = Write-Host "Enter the hostname prefix for new VM's:"
-$VmImageName = Write-Host "Enter the name of the Image:"
-$VmImageResourceGroup = Write-Host "Enter the Resource Group where the Image is located:"
-$Network_RG = Write-Host "Enter the Resource Group where the Vnet resides:"
-$Vnet = Write-Host "Enter the name of the Vnet new VM's will be added to:"
-$Subnet = Write-Host "Enter the subnet new VM's will be added to:"
-$NumberofVm = Write-Host "How many instances of the VM would you like created:"
+$deploymentRG = Read-Host "Enter Resource Group new VM's will be deployed to"
+$VMNamePrefix = Read-Host "Enter the hostname prefix for new VM's"
+$VmImageName = Read-Host "Enter the name of the Image"
+$VmImageResourceGroup = Read-Host "Enter the Resource Group where the Image is located"
+$Network_RG = Read-Host "Enter the Resource Group where the Vnet resides"
+$Vnet = Read-Host "Enter the name of the Vnet new VM's will be added to"
+$Subnet = Read-Host "Enter the subnet new VM's will be added to"
+$NumberofVm = Read-Host "How many instances of the VM would you like created"
 
 
 New-AzResourceGroupDeployment -TemplateFile "./azuredeploy.vmImage.json" `
